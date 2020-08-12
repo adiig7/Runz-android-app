9ab5105ca331f74a25d423c7caf21e4debf71792
adiig7 <54351909+adiig7@users.noreply.github.com>
2020-08-12 13:38:34 +0530

    new commit

---
+++ app/src/main/java/com/example/runz/db/RunDAO.kt
@@ -0,0 +1,31 @@
+package com.example.runz.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRun(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC")
+    fun getAllRunsSortedByCaloriesBurned(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH DESC")
+    fun getAllRunsSortedByavgSpeedInKMH(): LiveData<List<Run>>
+
+    @Query("SELECT  SUM(timestamp) FROM running_table")
+    fun getTotalTimeInMillis(): LiveData<List<Run>>
+
+
+
+}
